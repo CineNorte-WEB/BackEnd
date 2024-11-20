5aed2afe079120c3bdf2a4663229c103cce2b00b
tjdgus3488 <tjdgus3488@naver.com>
2024-11-20 17:25:50 +0900

    first commit

---
+++ src/main/java/com/tave/camchelin/CamchelinApplication.java
@@ -0,0 +1,13 @@
+package com.tave.camchelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamchelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamchelinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tave/camchelin/CamchelinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tave.camchelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamchelinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
