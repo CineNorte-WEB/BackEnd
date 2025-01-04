787f1163860a189a455646b4a3f508191900160f
tjdgus3488 <tjdgus3488@naver.com>
2025-01-04 15:24:38 +0900

    cors 설정 추가

--- src/main/java/com/tave/camchelin/global/config/SecurityConfig.java
+++ src/main/java/com/tave/camchelin/global/config/SecurityConfig.java
@@ -2,13 +2,10 @@ package com.tave.camchelin.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tave.camchelin.domain.users.repository.UserRepository;
-import com.tave.camchelin.global.jwt.JwtLogoutHandler;
-import com.tave.camchelin.global.jwt.JwtServiceImpl;
+import com.tave.camchelin.global.jwt.*;
 import com.tave.camchelin.global.security.JsonUsernamePasswordAuthenticationFilter;
 import com.tave.camchelin.global.security.LoginFailureHandler;
 import com.tave.camchelin.global.user.UserDetailsServiceImpl;
-import com.tave.camchelin.global.jwt.JwtAuthenticationProcessingFilter;
-import com.tave.camchelin.global.jwt.LoginSuccessJWTProvideHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +23,11 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -53,11 +55,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http    .csrf(AbstractHttpConfigurer::disable)
+                .cors()
+                .and()
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
 //                .addFilterAfter(jsonUsernamePasswordLoginFilter(), LogoutFilter.class)
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/login", "/").permitAll() // 비로그인 유저에게 권한을 어디까지 줄것인가?
+                        .requestMatchers("/login", "/", "/hello", "/users/register").permitAll() // 비로그인 유저에게 권한을 어디까지 줄것인가?
                         .anyRequest().authenticated())
                 // 폼 로그인
 //				.formLogin(formLogin -> formLogin
@@ -70,11 +74,25 @@ public class SecurityConfig {
                         .deleteCookies("JSESSIONID")) // 세션 무효화 및 쿠키 삭제
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         http
                 .addFilterAfter(jsonUsernamePasswordLoginFilter(), LogoutFilter.class) // 추가 : 커스터마이징 된 필터를 SpringSecurityFilterChain에 등록
                 .addFilterBefore(jwtAuthenticationProcessingFilter(), JsonUsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // 모든 Origin 허용
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 자격 증명 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        return source;
+    }
+
 
 //    @Bean
 //    public static BCryptPasswordEncoder bCryptPasswordEncoder(){
