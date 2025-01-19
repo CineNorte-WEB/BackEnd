66f1b5f20669a453b426d67653406633b07296d0
haeyoon1 <hynam1220@naver.com>
2025-01-19 10:53:41 +0900

    refactor: review_analysis 패키지 사용되지 않는 클래스 삭제

--- src/main/java/com/tave/camchelin/domain/review_analysis/repository/ReviewAnalysisStoreRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.tave.camchelin.domain.review_analysis.repository;
-//
-//import com.tave.camchelin.domain.review_analysis.entity.ReviewAnalysis;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ReviewAnalysisStoreRepository extends JpaRepository<ReviewAnalysis, Long> {
-//}
--- src/test/java/com/tave/camchelin/domain/review_analysis/controller/Model2ControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.tave.camchelin.domain.review_analysis.controller;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-class Model2ControllerTest {
-
-}
